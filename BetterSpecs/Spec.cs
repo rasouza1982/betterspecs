0321db9ddc5d1ce759c3fd0ab10bd42eba053aa6
Yan Justino <yanlimaj@gmail.com>
2015-02-05 09:18:56 -0300

    Add prefix on Spec

--- BetterSpecs/Spec.cs
+++ BetterSpecs/Spec.cs
@@ -7,30 +7,25 @@ namespace BetterSpecs
 {
     public class Spec
     {
+        private string _prefix;
         private int _indentation;
 
-        protected Spec(int indentation = 0)
+        protected Spec(int indentation = 0, string prefix = "")
         {
             this._indentation = indentation;
+            this._prefix = string.IsNullOrEmpty(prefix) ? string.Empty : prefix + " ";
         }
 
         public Action this[string message]
         {
             set 
             {
-                Speclog.Current.Write(message, _indentation);
+                Speclog.Current.Write(_prefix + message, _indentation);
                 value.Invoke(); 
             }
         }
     }
-
-    public class It : Spec
-    {
-        public It() : base(4) { }
-    }
-
-    public class Context : Spec
-    {
-        public Context() : base() { }
-    }
+    
+    public class It : Spec { public It() : base(4, "It") { } }
+    public class Context : Spec { public Context() : base() { } }
 }
